827e46f62565b5863cffb5899b794b8302d163cd
Hayet-mn <manaihayet48@gmail.com>
2020-01-15 19:42:03 +0100

    Initial commit from Create React App

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const {MongoClient, ObjectID} = require('mongodb')
+const bodyParser =require('body-parser')
+const assert =require('assert')
+
+
+const app=express()
+
+app.use(bodyParser.json())
+
+
+//definition de dataBase
+
+const mongo_url = "mongodb://localhost:27017"
+const dataBase = "contact-list"
+
+MongoClient.connect(mongo_url, (err, client)=> {
+    assert.equal(err, null, 'data base connexion failed')
+
+    const db = client.db(dataBase)
+
+    
+   
+//app.post : ajouter contact  document to collection in database
+    app.post('/new_contact', (req, res)=>{
+        let newContact = req.body 
+      db.collection('contacts').insertOne(newContact , (err, data)=>{
+          if(err) res.send('cannot add contact')
+          else res.send (data)
+      })
+})
+
+// retourne tous contact dans collection
+app.get('/contactList', (req, res)=>{
+    db.collection('contacts').find().toArray((err, data)=>{
+        if (err) res.send('cannot fetch contacts')
+        else res.send(data)
+    })
+})
+// retourne one contact by id dans collection
+app.get('/contact/:id', (req, res)=>{
+    let searchedContactID = ObjectID(req.params.id)
+    db.collection('contacts').findOne({_id : searchedContactID}, (err, data)=>{
+        if(err) res.send('cannot fetch contact')
+        else res.send(data)
+    })
+    })
+// modifier contact exist by id
+app.put('/modify_contact/:id' , (req, res) => {
+    let ContactToEditId = ObjectID(req.params.id)
+    let modifiedContact = req.body
+    db.collection('contacts').findOneAndUpdate({_id : ContactToEditId}, {$set:{...modifiedContact}}, (err, data) => {
+        if(err) res.send('cannot modify contact')
+        else res.send('contact was modified')
+    })
+})
+
+
+// delete contact exist by id
+app.delete('/delete_contact/:id', (req, res)=>{
+    let contactToRemoveId = ObjectID(req.params.id)
+    db.collection('contacts').findOneAndDelete({_id : contactToRemoveId}, (err, data) => {
+        if(err) res.send('cannot delete contact')
+        else res.send('contact was deleted')
+    })
+
+})
+
+
+})
+
+app.listen(5000, (err) => {
+    if(err) console.log("server error")
+    else console.log("server  is running on port 5000")
+
+})
